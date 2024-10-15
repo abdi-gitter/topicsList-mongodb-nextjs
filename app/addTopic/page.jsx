2a3db57a76240c80ae9ea6bdecab96ba730368bb
abdi-gitter <abdisat.bedada@gmail.com>
2024-10-15 13:13:33 -0500

    UI completed

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -8,8 +8,13 @@ export default function AddTopic(){
 
             <input className="border border-slate-500 px-8 py-2"
             type="text" 
-            placeholder="Topic Title"
+            placeholder="Topic Description"
         />
+
+        <button className="bg-green-600 font-bold
+            text-white py-3 px-6 w-fit">
+            Add Topic
+        </button>
         </form>
     );
 }
